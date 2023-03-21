b75e52c7bdbcb6e0f90917b2ad0e20b1e767378e
singhjagjit11 <singhjagji87@gmail.com>
2023-03-21 16:47:16 -0400

    new_config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-1_MX1zjZbgi", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "3jq7qohgj4kf8359ggj1pcvl46", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    userPoolId: "us-east-1_egoUOGkzb", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "4s4c7787as604i779q1qa6uti6", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: "us-east-1", // e.g. us-east-2
   },
   api: {
