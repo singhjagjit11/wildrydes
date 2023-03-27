2f75999513ae61aea81790a20f55715a337f21d4
singhjagjit11 <116024713+singhjagjit11@users.noreply.github.com>
2023-03-27 15:09:53 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
 
     cognito: {
 
-        userPoolId: 'us-west-2_uXboG5pAb', // e.g. us-east-2_uXboG5pAb         
+        userPoolId: 'us-east-1_egoUOGkzb', // e.g. us-east-2_uXboG5pAb         
 
-        userPoolClientId: '25ddkmj4v6hfsfvruhpfi7n4hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4s4c7787as604i779q1qa6uti6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
 
         region: 'us-west-2' // e.g. us-east-2 
 
