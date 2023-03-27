488f8506a31035214b575b2151dd11989cc99aad
singhjagjit11 <116024713+singhjagjit11@users.noreply.github.com>
2023-03-27 15:01:02 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,19 @@
 window._config = {
-  cognito: {
-    userPoolId: "us-east-1_egoUOGkzb", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "4s4c7787as604i779q1qa6uti6", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-    region: "us-east-1", // e.g. us-east-2
-  },
-  api: {
-    invokeUrl: "https://grspkumd6j.execute-api.us-east-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-  },
+
+    cognito: {
+
+        userPoolId: 'us-west-2_uXboG5pAb', // e.g. us-east-2_uXboG5pAb         
+
+        userPoolClientId: '25ddkmj4v6hfsfvruhpfi7n4hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+
+        region: 'us-west-2' // e.g. us-east-2 
+
+    }, 
+
+    api: { 
+
+        invokeUrl: 'https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod, 
+
+    } 
+
 };
