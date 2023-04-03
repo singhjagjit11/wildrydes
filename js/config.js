4c827a9c60e5e4c0a420e39205aff27a4fab7049
singhjagjit11 <116024713+singhjagjit11@users.noreply.github.com>
2023-04-03 11:54:12 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ window._config = {
 
         userPoolClientId: '4s4c7787as604i779q1qa6uti6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
 
-        region: 'us-west-2' // e.g. us-east-2 
+        region: 'us-east-1' // e.g. us-east-2 
 
     }, 
 
