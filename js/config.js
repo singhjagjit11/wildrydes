898132276ec60e844231d3a79385feefe93c9ab2
singhjagjit11 <116024713+singhjagjit11@users.noreply.github.com>
2023-04-03 12:53:48 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -12,7 +12,7 @@ window._config = {
 
     api: { 
 
-        invokeUrl: 'https://9u4szde7ie.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod, 
+        invokeUrl: 'https://9u4szde7ie.execute-api.us-east-1.amazonaws.com/c' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod, 
 
     } 
 
